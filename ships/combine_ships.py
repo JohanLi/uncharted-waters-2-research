dce413c2277d0f0d91150a73db495785c64b2976
Johan Li <hi@johan.li>
2022-01-19 15:45:06 +0100

    script for combining all ships

---
+++ ships/combine_ships.py
@@ -0,0 +1,42 @@
+import pathlib
+from PIL import Image, ImageDraw
+
+pathlib.Path('./output').mkdir(parents=True, exist_ok=True)
+
+ships = [
+  "Balsa",
+  "Hansa Cog",
+  "Dhow",
+  "Buss",
+  "Tallette",
+  "Caravela Latina",
+  "Caravela Redonda",
+  "Brigantine",
+  "Nao",
+  "Carrack",
+  "Galleon",
+  "Xebec",
+  "Pinnace",
+  "Sloop",
+  "Frigate",
+  "Barge",
+  "Full-rigged Ship",
+  "Junk",
+  "Light Galley",
+  "Flemish Galleon",
+  "Venetian Galeass",
+  "La Reale",
+  "Tekkousen",
+  "Atakabune",
+  "Kansen"
+]
+
+im = Image.new('RGBA', (128 * len(ships), 96), (255, 255, 255, 0))
+
+for i, ship in enumerate(ships):
+  filename = ship.replace(' ', '-').lower()
+
+  shipIm = Image.open(f'./ships/img/{filename}.png', 'r')
+  im.paste(shipIm, (128 * i, 0))
+
+im.save('./output/combined-ships.png')
